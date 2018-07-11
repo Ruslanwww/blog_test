b51674540a61814ae5b4ea7d5d5c5eb69e049a9b
Ruslanwww <oruslano51@gmail.com>
2018-07-11 17:05:04 +0300

    Add articles show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new  
   end
 
@@ -6,10 +11,13 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     if @article.valid?
       @article.save
+      redirect_to @article
     else
       render action: 'new' 
     end
   end
+
+  private
 
   def article_params
     params.require(:article).permit(:title, :text)
