4c3c3273ae51f58d947b042c20e756bd9a8a96ff
Ruslanwww <oruslano51@gmail.com>
2018-07-18 12:38:56 +0300

    Install rspec(2.8)

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module Blog
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework   :rspec, :fixture => true, :views => false
+      g.integration_tool :rspec, :fixture => true, :views => true
+    end
   end
 end
